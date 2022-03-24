c925f274da284f26a5cae36c464b1371e95f1649
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-24 08:12:07 +0545

    react state and side effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+console.log('I am starting your application!');
 
 function App() {
+  console.log('I have just entered into App component!');
+  const [state, setState] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('myCat', 'Hello I am from localstorage');
+  });
+
+  console.log(state);
+
+  function handleChange(e) {
+    console.log('I have just entered into handleChange function');
+    setState(e.target.value);
+    console.log(state);
+  }
+
+  function handleClick() {
+    setState('hello');
+  }
+
+  console.log(state);
+  console.log('I am entering into JSX!');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Searching...</h1>
+      <label htmlFor='search'>Search: </label>
+      <input type='text' id='search' onChange={handleChange} />
+
+      <button onClick={handleClick}>Click Me</button>
+
+      <h2>Searching for: {state.a}</h2>
     </div>
   );
 }
