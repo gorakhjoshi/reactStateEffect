67ad87520bdbcfdc84a211b2a31c1ffe6bbd4fcd
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-25 07:29:51 +0545

    useEffect with localstorage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import List from './List';
 import { stories } from './stories';
 
 function App() {
-  const [state, setState] = useState('');
+  const [state, setState] = useState(localStorage.getItem('myCat') || 'React');
 
   useEffect(() => {
-    localStorage.setItem('myCat', 'Hello I am from localstorage');
+    localStorage.setItem('myCat', state);
   });
 
   function handleChange(e) {
@@ -21,9 +21,10 @@ function App() {
     <div>
       <h1>Searching...</h1>
       <label htmlFor='search'>Search: </label>
-      <input type='text' id='search' onChange={handleChange} />
+      <input type='text' id='search' onChange={handleChange} value={state} />
       <button onClick={handleClick}>Click Me</button>
       <h2>Searching for: {state.a}</h2>
+      {state}
       <hr />
       <List stories={stories}>hello</List>
     </div>
