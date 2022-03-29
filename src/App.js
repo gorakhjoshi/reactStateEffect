b4b776c71f6d36b2aa0a4d2dce5de7df73e13e58
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-29 08:42:05 +0545

    Context API in react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { CountDisplay } from './components/CountDisplay';
 import { Counter } from './components/Counter';
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <CountDisplay />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
