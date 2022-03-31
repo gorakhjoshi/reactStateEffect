c950d739f39edbf339e4a5a1278c31c8c9e5f81c
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-31 06:32:00 +0545

    global css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
